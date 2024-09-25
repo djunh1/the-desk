6434e6475a40786b62f9e3e362e90da080820ab3
djunh1 <djunh1@gmail.com>
2024-09-25 17:51:49 -0400

    Remove base URL

--- src/tdesk/settings.py
+++ src/tdesk/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
-BASE_URL = config("BASE_URL", default=None)
+#BASE_URL = config("BASE_URL", default=None)
 
 ALLOWED_HOSTS = [
     ".railway.app"
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tdesk.urls'
-print("base dir", BASE_DIR)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
